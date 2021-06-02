98e3bdef1e94d7fb23dcde85fe78e1a76b8f5eb9
marcos-x86 <marcos.x86@outlook.com>
2021-06-02 15:23:26 -0400

    Base projects improvements

--- src/test/java/com/jalasoft/bdd/runner/Runner.java
+++ src/test/java/com/jalasoft/bdd/runner/Runner.java
@@ -2,6 +2,9 @@ package com.jalasoft.bdd.runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 
 /**
  * Defines Cucumber Runner.
@@ -13,4 +16,28 @@ import io.cucumber.testng.CucumberOptions;
 )
 public class Runner extends AbstractTestNGCucumberTests {
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+
+    /**
+     * Executes actions before all scenarios.
+     */
+    @BeforeTest
+    public void beforeAllScenarios() {
+        System.setProperty("dataproviderthreadcount", "2");
+    }
+
+    /**
+     * Executes actions after all scenarios.
+     */
+    @AfterTest
+    public void afterAllScenarios() {
+
+    }
 }
